d21d2541dcfe914f820efbb68c9982d93ce1f879
Glib Briia <glib.briia@gmail.com>
2018-11-21 23:55:20 +0000

    Fix key name

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_1592987b81cf_key -iv $encrypted_1592987b81cf_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_1592987b81cf_key -iv $encrypted_1592987b81cf_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
